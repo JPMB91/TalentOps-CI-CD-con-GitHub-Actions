492e3b69f46cdd6863db72c5f87196318d4b93c3
Juan Maldonado <juan.maldonado@live.cl>
2026-01-14 19:47:18 -0300

    test: add Hello netlify message to test pipeline

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@ export default function ProfilePage() {
     <div className="min-h-screen p-8">
       <h1 className="text-3xl font-bold mb-4">User Profile</h1>
       <div className="bg-gray-100 p-6 rounded-lg">
+        <p className="text-gray-700">Hello from Netlify</p>
         <p className="text-gray-700">User profile information: </p>
       </div>
     </div>
