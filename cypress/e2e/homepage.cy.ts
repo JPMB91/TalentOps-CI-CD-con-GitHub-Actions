f25a2f96808d641f2ab6c85c553a0fa0a042e909
Juan Maldonado <juan.maldonado@live.cl>
2026-01-14 16:04:52 -0300

    chore: configure prettier & cypress

---
+++ cypress/e2e/homepage.cy.ts
@@ -0,0 +1,11 @@
+describe('Homepage E2E', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+
+  it('debe navegar a /profile', () => {
+    cy.visit('http://localhost:3000/profile');
+    cy.contains('User Profile').should('be.visible');
+  });
+});
